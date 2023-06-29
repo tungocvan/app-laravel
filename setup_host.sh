a1f954d2c4311f0971e4bb6215a5b168defb1a42
tungocvan <tungocvan@gmail.com>
2023-06-29 14:02:14 +0700

    setup-2

--- setup_host.sh
+++ setup_host.sh
@@ -1,10 +1,7 @@
 mkdir database/migrations
-echo "nhấn enter tiếp tục"
 composer update
-echo "nhấn enter tiếp tục"
 cp modules/Auth/src/Http/Controllers/AuthRouteMethods.php vendor/laravel/ui/src
-echo "nhấn enter tiếp tục"
 mv .env_host .env
 echo "hoàn thành cài đặt"
-echo "Cài đặt table user"
+echo "Lưu ý setup database và cài đặt table user"
 echo "php artisan migrate:fresh"
